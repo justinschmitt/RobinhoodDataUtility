0ac02c9b13b17b2c177fc4cf3109e2d18a9b079f
justin <justin@DESKTOP>
2017-02-28 23:42:55 -0500

    refactored and switched to Newtonsoft.json

--- RobinhoodDataUtility/RobinhoodDataUtility/MainWindow.xaml.cs
+++ RobinhoodDataUtility/RobinhoodDataUtility/MainWindow.xaml.cs
@@ -5,74 +5,43 @@ using System.Windows;
 using System.Web.Script.Serialization;
 using System.Reflection;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace RobinhoodDataUtility
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        String token = null;
+        Robinhood connection = new Robinhood();
 
         public MainWindow()
         {
             InitializeComponent();
+            connection.ConnectionEstablished += Connection_ConnectionEstablished;
         }
 
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        private void Connection_ConnectionEstablished(object sender, EventArgs e)
         {
-            if (tbUsername.Text.Length != 0 && pbPassword.Password.Length != 0)
-            {
-                Login(tbUsername.Text, pbPassword.Password);
-            }
-            else
-            {
-                MessageBox.Show("You must enter a Username and Password");
-            }
+            LoginTab.IsEnabled = false;
+            ToolTab.IsEnabled = true;
+            ToolTab.Focus();
         }
 
-        public async void Login(string username, string password)
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            using (HttpClient client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("https://api.robinhood.com/");
-                var request = new HttpRequestMessage(HttpMethod.Post, "api-token-auth/");
-                client.DefaultRequestHeaders.Add("Accept", "*/*"); 
-                client.DefaultRequestHeaders.Add("Accept-Language", "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5");
-                client.DefaultRequestHeaders.Add("X-Robinhood-API-Version", "1.0.0");
-                client.DefaultRequestHeaders.Connection.Add("keep-alive");
-                client.DefaultRequestHeaders.Add("User-Agent", "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)");
-
-                List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>>();
-                data.Add(new KeyValuePair<string, string>("username", username));
-                data.Add(new KeyValuePair<string, string>("password", password));
-
-                request.Content = new FormUrlEncodedContent(data);
-                HttpResponseMessage response = await client.SendAsync(request);
-                String responseContent = await response.Content.ReadAsStringAsync();
-
-
-                try
-                {
-                    var jss = new JavaScriptSerializer();
-
-                    Wrappers.ApiTokenAuth auth = jss.Deserialize<Wrappers.ApiTokenAuth>(responseContent);
-
-                    token = auth.token;
-
-
-                    Console.WriteLine(token);
-                    lblStatus.Content = "Logged in!";
-                    
-                }
-                catch
-                {
-                    MessageBox.Show("Problem logging in: " + responseContent);
-                }
-
-
+                await connection.Login(tbUsername.Text, pbPassword.Password);
+                tbStatus.Text = "Logged in!";
             }
+            catch(ConnectionException ex)
+            {
+                MessageBox.Show(ex.Message);
+                tbStatus.Text = "Login Failed";
+            }  
         }
 
 
@@ -83,71 +52,42 @@ namespace RobinhoodDataUtility
 
         public async void GetOrders()
         {
-            using (HttpClient client = new HttpClient())
+
+            Wrappers.Orders orders = JsonConvert.DeserializeObject<Wrappers.Orders>(await connection.GetEndpoint("orders"));
+
+            tbStatus.Text = "Got Orders!";
+
+            String s = "";
+
+            //header row
+            Type type = orders.results[0].GetType();
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo p in properties)
             {
-                client.BaseAddress = new Uri("https://api.robinhood.com/");
-                var request = new HttpRequestMessage(HttpMethod.Get, "orders/");
-                client.DefaultRequestHeaders.Add("Accept", "*/*");
-                client.DefaultRequestHeaders.Add("Accept-Language", "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5");
-                client.DefaultRequestHeaders.Add("X-Robinhood-API-Version", "1.0.0");
-                client.DefaultRequestHeaders.Connection.Add("keep-alive");
-                client.DefaultRequestHeaders.Add("User-Agent", "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)");
-                client.DefaultRequestHeaders.Add("Authorization", "Token " + token);
+                s += p.Name + ",";
+            }
 
+            s += "\n";
 
-                HttpResponseMessage response = await client.SendAsync(request);
-                String responseContent = await response.Content.ReadAsStringAsync();
+            foreach (Wrappers.Result order in orders.results)
+            {
+                Type t = order.GetType();
+                PropertyInfo[] pi = t.GetProperties();
 
-
-                try
+                foreach (PropertyInfo p in pi)
                 {
-                    var jss = new JavaScriptSerializer();
-
-                    Wrappers.Orders orders = jss.Deserialize<Wrappers.Orders>(responseContent);
-
-
-                    Console.WriteLine(orders.results.ToString());
-                    lblStatus.Content = "Got Orders!";
-
-                    String s = "";
-
-                    //header row
-                    Type type = orders.results[0].GetType();
-                    PropertyInfo[] properties = type.GetProperties();
-                    foreach (PropertyInfo p in properties)
-                    {
-                        s += p.Name + ",";
-                    }
-
-                    s += "\n";
-
-                    foreach (Wrappers.Result order in orders.results)
-                    {
-                        Type t = order.GetType();
-                        PropertyInfo[] pi = t.GetProperties();
-
-                        foreach (PropertyInfo p in pi)
-                        {
-                            s += p.GetValue(order, null) + ",";
-                        }
-                        s += "\n";
-                    }
-
-                    using (StreamWriter outputFile = new StreamWriter(System.IO.Path.GetFullPath("OrderExport.csv")))
-                    {
-                        await outputFile.WriteAsync(s);
-
-                    }
-
-                    MessageBox.Show("file saved!");
+                    s += p.GetValue(order, null) + ",";
                 }
-                catch (Exception e)
-                {
-                    MessageBox.Show("Problem getting orders: " + responseContent);
-                }
+                s += "\n";
+            }
 
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.GetFullPath("OrderExport.csv")))
+            {
+                await outputFile.WriteAsync(s);
 
             }
+
+            MessageBox.Show("file saved!");
         }
     }
 }
---
+++ RobinhoodDataUtility/RobinhoodDataUtility/Robinhood.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using RobinhoodDataUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+
+namespace RobinhoodDataUtility
+{
+    public class Robinhood
+    {
+        private string token = null;
+
+        public Boolean Connected { get { return token != null; } }
+
+        public event EventHandler ConnectionEstablished;
+
+        public async Task Login(string username, string password)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.robinhood.com/");
+                var request = new HttpRequestMessage(HttpMethod.Post, "api-token-auth/");
+                client.DefaultRequestHeaders.Add("Accept", "*/*");
+                client.DefaultRequestHeaders.Add("Accept-Language", "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5");
+                client.DefaultRequestHeaders.Add("X-Robinhood-API-Version", "1.0.0");
+                client.DefaultRequestHeaders.Connection.Add("keep-alive");
+                client.DefaultRequestHeaders.Add("User-Agent", "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)");
+
+                List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>>();
+                data.Add(new KeyValuePair<string, string>("username", username));
+                data.Add(new KeyValuePair<string, string>("password", password));
+
+                request.Content = new FormUrlEncodedContent(data);
+                HttpResponseMessage response = await client.SendAsync(request);
+                string loginResponse = await response.Content.ReadAsStringAsync();
+
+                try
+                {
+                    var jss = new JavaScriptSerializer();
+                    Wrappers.ApiTokenAuth auth = jss.Deserialize<Wrappers.ApiTokenAuth>(loginResponse);
+                    token = auth.token;
+                }
+                catch
+                {
+                    throw new ConnectionException(loginResponse);
+                }
+
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    throw new ConnectionException(loginResponse);
+                }
+
+                if (token != null)
+                {
+                    this.ConnectionEstablished(this, null);
+                }
+                else
+                {
+                    throw new ConnectionException(loginResponse);
+                }
+            }
+        }
+
+        public async Task<string> GetEndpoint(string endpoint)
+        {
+            if (!Connected) throw new ConnectionException("Cannot request endpoint: " + endpoint + " Not currently connected.");
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.robinhood.com/");
+                var request = new HttpRequestMessage(HttpMethod.Get, endpoint + "/");
+                client.DefaultRequestHeaders.Add("Accept", "*/*");
+                client.DefaultRequestHeaders.Add("Accept-Language", "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5");
+                client.DefaultRequestHeaders.Add("X-Robinhood-API-Version", "1.0.0");
+                client.DefaultRequestHeaders.Connection.Add("keep-alive");
+                client.DefaultRequestHeaders.Add("User-Agent", "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)");
+                client.DefaultRequestHeaders.Add("Authorization", "Token " + token);
+
+                HttpResponseMessage response = await client.SendAsync(request);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+    }
+
+    class ConnectionException : Exception
+    {
+        public ConnectionException(string message)
+        : base(message) { }
+    }
+}
